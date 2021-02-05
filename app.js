7e7c13497691107adbcd8a8e6affdb47e94e7c4d
ethanjd23 <ethanjd23@gmail.com>
2021-02-04 20:13:00 -0600

    can't get the game to restart

--- app.js
+++ app.js
@@ -13,12 +13,36 @@ $("document").ready(function() {
         };
     });
     // Toggling upper and lower case keyboards on shift ^
-
+    
     $("body").keypress(function(e) {
-        $("#" + e.which).css("background-color", "yellow");
+        let keyPressed = e.which
+        $("#" + keyPressed).css("background-color", "yellow");
+        setTimeout(function() {
+            $("#" + keyPressed).css("background-color", "#f5f5f5")
+        }, 100);
     });
-
+    // highlighting keys when pressed
+    
+    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+    console.log(sentences.length)
+    let currentSentence = 0;
+    $("#sentence").append(sentences[0]);
+    let keyCount = 0;
     $("body").keyup(function(e) {
-        $("#" + e.which).css("background-color", "#f5f5f5");
-    });
+        keyCount++;
+        if (currentSentence > sentences.length) {
+            $("#sentence").empty();
+            currentSentence = 0; 
+            keyCount = 0; 
+            $("#sentence").append(sentences[0]);
+            return;
+        }
+        if(keyCount == sentences[currentSentence].length) {
+            currentSentence++;
+            keyCount = 0;
+            $("#sentence").empty();
+            $("#sentence").append(sentences[currentSentence]);
+            console.log(currentSentence)
+        }; 
+    })
 })
